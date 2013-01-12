80e941ad99b5bca9ee235c132af53997f39703d1
Nick Dimiduk <ndimiduk@gmail.com>
2013-01-12 00:33:24 -0800

    update dependencies to hbase-0.94.x and hadoop-core-1.0.x

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
              {:dependencies
               [[midje "1.3.1" :exclusions [org.clojure/clojure]]
                [org.clojure/clojure "1.2.1"]
-               [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+               [org.apache.hadoop/hadoop-core "1.0.3"]
                [asm "3.2"]
-               [org.apache.hbase/hbase "0.90.2"
+               [org.apache.hbase/hbase "0.94.3"
                 :exclusions [org.apache.hadoop/hadoop-core asm]]
                [cascading/cascading-hadoop "2.0.0"
                 :exclusions [org.codehaus.janino/janino
